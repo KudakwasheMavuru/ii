5e77c72908b898f1e04199d9b082a3e9b3fb58aa
KudakwasheMavuru <43370333+KudakwasheMavuru@users.noreply.github.com>
2021-05-17 12:53:30 +0200

    Add files via upload

---
+++ js/custom2.js
@@ -0,0 +1,24 @@
+var btn = $('#buton');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+(function($){
+  'use strict';
+    $(window).on('load', function () {
+        if ($(".pre-loader").length > 0)
+        {
+            $(".pre-loader").fadeOut("slow");
+        }
+    });
+})(jQuery)
